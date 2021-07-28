f2b9e709f75c437075ef612ff404708eb9b7a451
Priyanka Patel <83409681+Priyanka-Cloud-Infinite@users.noreply.github.com>
2021-07-28 13:57:25 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-c=$(ls -l /var/www/html/ | wc -l)
+c=$(ls -l /var/www/testing/ | wc -l)
 
 if [ $c -gt 0 ]
 then
-  cd /var/www/html
+  cd /var/www/testing
   
   rm -fr *
   rm -fr .git*
